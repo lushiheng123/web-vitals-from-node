b67a3a194feb40774b6c138805fda99819eb14b3
unknown <robertpierce2828@gmail.com>
2024-06-21 10:48:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# echo "# web-vitals-from-node" >> README.md
+
+# git init
+
+# git add README.md
+
+# git commit -m "first commit"
+
+# git branch -M main
+
+# git remote add origin git@github.com:lushiheng123/web-vitals-from-node.git
+
+# git push -u origin main
---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState, useRef, useEffect } from "react";
+
+import Menu from "./Menu";
+function App() {
+  const [brushColor, setBrushColor] = useState("green");
+  const [brushWidth, setBrushWidth] = useState(8);
+  const [brushOpacity, setBrushOpacity] = useState(0.5);
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isDraw, setIsDraw] = useState(false);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoine = "round";
+    ctx.globalAlpha = brushOpacity;
+    ctx.strokeStyle = brushColor;
+
+    ctx.lineWidth = brushWidth;
+    ctxRef.current = ctx;
+  }, [brushColor, brushWidth, brushOpacity]);
+  const startDraw = (e) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    setIsDraw(true);
+  };
+
+  const endDraw = (e) => {
+    ctxRef.current.closePath();
+    setIsDraw(false);
+  };
+
+  const draw = (e) => {
+    if (!isDraw) {
+      return;
+    }
+    ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+
+    ctxRef.current.stroke();
+  };
+  return (
+    <div className="App">
+      <h1>Paint App</h1>
+      <div className="draw-area">
+        <Menu
+          setBrushColor={setBrushColor}
+          setBrushOpacity={setBrushOpacity}
+          setBrushWidth={setBrushWidth}
+        />
+
+        <canvas
+          width="1200px"
+          height="500px"
+          ref={canvasRef}
+          onMouseDown={startDraw}
+          onMouseUp={endDraw}
+          onMouseMove={draw}
+        ></canvas>
+      </div>
+    </div>
+  );
+}
+
+export default App;
